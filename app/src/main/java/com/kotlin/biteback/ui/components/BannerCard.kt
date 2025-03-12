bafc1ac918710d4ecb0a0bbfa6b8aa11b572d53f
DannyC666 <d.munozs@uniandes.edu.co>
2025-03-12 09:49:19 -0500

    feat: BannerCard features

--- app/src/main/java/com/kotlin/biteback/ui/components/BannerCard.kt
+++ app/src/main/java/com/kotlin/biteback/ui/components/BannerCard.kt
@@ -1,5 +1,7 @@
 package com.kotlin.biteback.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,5 +16,13 @@ fun BannerCard(){
     Card(
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        modifier = Modifier.height(250.dp).width(220.dp)) { }
+        modifier = Modifier.height(250.dp).width(220.dp)) {
+
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ){
+
+        }
+
+    }
 }
